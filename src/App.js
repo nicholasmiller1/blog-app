947be4caee8cfeca0a8a757c7a5d1256f5b6920e
Nicholas Miller <nicholascmiller123@gmail.com>
2021-04-11 15:41:54 -0400

    Added authentication/login system for firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
+import firebase, {displayAuthUi} from './firebase.js';
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    displayAuthUi("#firebase-authui")
+    firebase.firestore().collection("posts").get().then((response) => {
+      const arr = [];
+      response.forEach(res => arr.push(res.data()));
+      setData(arr);
+    });
+  }, [])
+
+  const handleClick = () => {
+    firebase.firestore().collection("posts").add({
+      userId: "test user 2",
+      text: "test content 2",
+      date: "04/06/2021"
+    })
+  }
+
   return (
     <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      {data !== undefined && data.map(post => 
+      <div style={{ width: "200px", backgroundColor: "#e7e7e7", borderRadius: "5px", padding: "15px", margin: "20px auto", boxShadow: "5px 10px #888888" }}>
+        <h1>{post.userId}</h1>
+        <p>{post.text}</p>
+        <p>{post.date}</p>
+      </div>)}
+      <div id="firebase-authui"></div>
+      <button onClick={handleClick}>Post</button>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase';
+import * as firebaseui from 'firebaseui';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB3WNPn7pspn8ZLkpLZyFUHT0-d1J6u7vA",
+    authDomain: "authenticated-blog-app.firebaseapp.com",
+    projectId: "authenticated-blog-app",
+    storageBucket: "authenticated-blog-app.appspot.com",
+    messagingSenderId: "606170293718",
+    appId: "1:606170293718:web:6d1356f6903b2c167b7077"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const uiConfig = {
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ]
+}
+
+const authui = new firebaseui.auth.AuthUI(firebase.auth());
+const displayAuthUi = (elementId) => {
+    authui.start(elementId, uiConfig);
+}
+
+export {displayAuthUi};
+export default firebase;
